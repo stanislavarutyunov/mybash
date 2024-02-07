907267da24e28497f76640d82ea94a82f14c2006
YaroslavIT <ityaroslav@gmail.com>
2024-02-07 17:49:55 +0300

    Update syslogng.sh
    Change path to /etc/syslog-ng/conf.d/

--- scripts/syslogng.sh
+++ scripts/syslogng.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 MP_IP_ADDRESS=""
 # Создание файла 10-siem.conf и добавление в него необходимых настроек 
-sudo touch /etc/syslog-ng/10-siem.conf
-sudo chmod 666 /etc/syslog-ng/10-siem.conf
+sudo touch /etc/syslog-ng/conf.d/10-siem.conf
+sudo chmod 666 /etc/syslog-ng/conf.d/10-siem.conf
 
 echo 'filter pt_siem_filter { not facility(mail, lpr, news, uucp, cron); };
 destination siem_agent_udp { udp("${MP_IP_ADDRESS}" port(514)); };
-log { source(s_src); filter(pt_siem_filter); destination(siem_agent_udp); };' | sudo tee -a /etc/syslog-ng/10-siem.conf
+log { source(s_src); filter(pt_siem_filter); destination(siem_agent_udp); };' | sudo tee -a /etc/syslog-ng/conf.d/10-siem.conf
 
 # Добавление строки @include "10-siem.conf" в конфигурационный файл syslog-ng 
-sudo sed -i '/@include "scl.conf";/a @include "10-siem.conf";' /etc/syslog-ng/syslog-ng.conf
+# sudo sed -i '/@include "scl.conf";/a @include "10-siem.conf";' /etc/syslog-ng/syslog-ng.conf
 
 # Перезапуск службы syslog-ng
 sudo systemctl restart syslog-ng.service
